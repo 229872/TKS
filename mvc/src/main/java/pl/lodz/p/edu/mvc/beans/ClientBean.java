48696e0caa0f9debd224fcd1106db8b4fdb999ea
Firedog01 <jan.rubacha@onet.pl>
2022-12-02 13:20:04 +0100

    wip client list

---
+++ mvc/src/main/java/pl/lodz/p/edu/mvc/beans/ClientBean.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.edu.mvc.beans;
+
+import pl.lodz.p.edu.data.model.DTO.users.ClientDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClientBean {
+
+    public List<ClientDTO> getAllActive() {
+        return new ArrayList<>();
+    }
+}

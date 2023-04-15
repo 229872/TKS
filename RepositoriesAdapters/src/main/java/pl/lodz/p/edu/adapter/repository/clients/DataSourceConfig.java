0c18550e7f006cfe06ff84edfa66cbdfd6f5a813
229872 Dygasiński Bartłomiej <229872@edu.p.lodz.pl>
2023-04-15 14:35:35 +0200

    Test repo now has own database

--- RepositoriesAdapters/src/main/java/pl/lodz/p/edu/adapter/repository/clients/DataSourceConfig.java
+++ RepositoriesAdapters/src/main/java/pl/lodz/p/edu/adapter/repository/clients/DataSourceConfig.java
@@ -1,10 +1,18 @@
 package pl.lodz.p.edu.adapter.repository.clients;
 
 import jakarta.annotation.sql.DataSourceDefinition;
+import jakarta.enterprise.context.ApplicationScoped;
 
-@DataSourceDefinition(name = "java:global/PgDataSource", className = "org.postgresql.ds.PGSimpleDataSource",
-url = "jdbc:postgresql://db:5432/nbddb", user = "nbd", password = "nbdpassword")
-//@DataSourceDefinition(name = "java:global/test", className = "org.postgresql.ds.PGSimpleDataSource",
-//url = "jdbc:postgresql://localhost:5432/nbddb", user = "nbd", password = "nbdpassword")
+import java.sql.Connection;
+
+@ApplicationScoped
+@DataSourceDefinition(
+        name = "java:global/PgDataSource",
+        className = "org.postgresql.ds.PGSimpleDataSource",
+        url = "jdbc:postgresql://db:5432/nbddb",
+        user = "nbd",
+        password = "nbdpassword",
+        isolationLevel = Connection.TRANSACTION_READ_COMMITTED
+)
 public class DataSourceConfig {
 }

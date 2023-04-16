84fc8dab6e8ea9b55fc1db2f070d16332331d5ee
229872 Dygasiński Bartłomiej <229872@edu.p.lodz.pl>
2023-04-16 20:41:02 +0200

    działa

--- SoapControllersAdapters/src/test/java/soap/SoapExampleIT.java
+++ SoapControllersAdapters/src/test/java/soap/SoapExampleIT.java
@@ -2,7 +2,7 @@ package soap;
 
 import org.junit.jupiter.api.Test;
 import org.testcontainers.junit.jupiter.Testcontainers;
-//import pl.soap.*;
+import pl.soap.*;
 
 
 @Testcontainers
@@ -13,71 +13,71 @@ public class SoapExampleIT extends AppDeploymentTestConfig {
         System.out.println("test ok");
     }
 
-//    EquipmentInputSoapDTO eq = new EquipmentInputSoapDTO();
-//
-//    @BeforeEach
-//    void beforeEach() {
-//        eq.setName(randStr(10));
-//        eq.setBail(2);
-//        eq.setFirstDayCost(1);
-//        eq.setNextDaysCost(3);
-////        eq.setDescription("seven");
-//    }
-//
-//    @Test
-//    void createEmployee_correct() {
-//        EquipmentSoapControllerService soap = new EquipmentSoapControllerService();
-//
-//        int initSize = soap.getEquipmentSoapControllerPort().getAll().size();
-//        soap.getEquipmentSoapControllerPort().add(eq);
-//        assertEquals(initSize + 1, soap.getEquipmentSoapControllerPort().getAll().size());
-//    }
-//
-//    @Test
-//    void removeEmployee_correct() {
-//        EquipmentSoapControllerService soap = new EquipmentSoapControllerService();
-//
-//        int initSize = soap.getEquipmentSoapControllerPort().getAll().size();
-//        var eq1 = soap.getEquipmentSoapControllerPort().add(eq);
-//        assertEquals(initSize + 1, soap.getEquipmentSoapControllerPort().getAll().size());
-//
-//        try {
-//            soap.getEquipmentSoapControllerPort().remove(eq1.getEntityId());
-//        } catch(ObjectNotFoundSoapException_Exception | SoapConflictException_Exception e) {
-//            fail();
-//        }
-//    }
-//
-//    @Test
-//    void updateEmployee_correct() throws ObjectNotFoundSoapException_Exception {
-//        EquipmentSoapControllerService soap = new EquipmentSoapControllerService();
-//
-//        int initSize = soap.getEquipmentSoapControllerPort().getAll().size();
-//        var eq1 = soap.getEquipmentSoapControllerPort().add(eq);
-//        assertEquals(initSize + 1, soap.getEquipmentSoapControllerPort().getAll().size());
-//
-//        String newName = "__1__";
-//        eq.setName(newName);
-//        try {
-//            soap.getEquipmentSoapControllerPort().update(eq1.getEntityId(), eq);
-//        } catch(ObjectNotFoundSoapException_Exception | SoapIllegalModificationException_Exception e) {
-//            fail();
-//        }
-//        var eq2 = soap.getEquipmentSoapControllerPort().get(eq1.getEntityId());
-//        assertEquals(newName, eq2.getName());
-//    }
-//
-//
-//    private static String randStr(int size) {
-//        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-//                + "0123456789"
-//                + "abcdefghijklmnopqrstuvxyz";
-//
-//        StringBuilder sb = new StringBuilder(size);
-//        for (int i = 0; i < size; i++) {
-//            int index = (int) (AlphaNumericString.length() * Math.random());
-//            sb.append(AlphaNumericString.charAt(index));
-//        }
-//        return sb.toString();
-//    }
+    EquipmentInputSoapDTO eq = new EquipmentInputSoapDTO();
+
+    @BeforeEach
+    void beforeEach() {
+        eq.setName(randStr(10));
+        eq.setBail(2);
+        eq.setFirstDayCost(1);
+        eq.setNextDaysCost(3);
+//        eq.setDescription("seven");
+    }
+
+    @Test
+    void createEmployee_correct() {
+        EquipmentSoapControllerService soap = new EquipmentSoapControllerService();
+
+        int initSize = soap.getEquipmentSoapControllerPort().getAll().size();
+        soap.getEquipmentSoapControllerPort().add(eq);
+        assertEquals(initSize + 1, soap.getEquipmentSoapControllerPort().getAll().size());
+    }
+
+    @Test
+    void removeEmployee_correct() {
+        EquipmentSoapControllerService soap = new EquipmentSoapControllerService();
+
+        int initSize = soap.getEquipmentSoapControllerPort().getAll().size();
+        var eq1 = soap.getEquipmentSoapControllerPort().add(eq);
+        assertEquals(initSize + 1, soap.getEquipmentSoapControllerPort().getAll().size());
+
+        try {
+            soap.getEquipmentSoapControllerPort().remove(eq1.getEntityId());
+        } catch(ObjectNotFoundSoapException_Exception | SoapConflictException_Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    void updateEmployee_correct() throws ObjectNotFoundSoapException_Exception {
+        EquipmentSoapControllerService soap = new EquipmentSoapControllerService();
+
+        int initSize = soap.getEquipmentSoapControllerPort().getAll().size();
+        var eq1 = soap.getEquipmentSoapControllerPort().add(eq);
+        assertEquals(initSize + 1, soap.getEquipmentSoapControllerPort().getAll().size());
+
+        String newName = "__1__";
+        eq.setName(newName);
+        try {
+            soap.getEquipmentSoapControllerPort().update(eq1.getEntityId(), eq);
+        } catch(ObjectNotFoundSoapException_Exception | SoapIllegalModificationException_Exception e) {
+            fail();
+        }
+        var eq2 = soap.getEquipmentSoapControllerPort().get(eq1.getEntityId());
+        assertEquals(newName, eq2.getName());
+    }
+
+
+    private static String randStr(int size) {
+        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                + "0123456789"
+                + "abcdefghijklmnopqrstuvxyz";
+
+        StringBuilder sb = new StringBuilder(size);
+        for (int i = 0; i < size; i++) {
+            int index = (int) (AlphaNumericString.length() * Math.random());
+            sb.append(AlphaNumericString.charAt(index));
+        }
+        return sb.toString();
+    }
 }
